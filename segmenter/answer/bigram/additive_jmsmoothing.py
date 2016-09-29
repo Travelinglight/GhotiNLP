c5d40ab073ae9f54db0dfaa3b195668932f79ffc
Jingda Chen <holaelmundokingston@gmail.com>
2016-09-29 12:18:51 -0700

    additive smoothing dp improved

--- segmenter/answer/bigram/additive_jmsmoothing.py
+++ segmenter/answer/bigram/additive_jmsmoothing.py
@@ -3,24 +3,23 @@ from __future__ import print_function
 from heapq import heappush, heappop
 import sys, codecs, optparse, os, math, unicodedata
 
-## k = 0 unigram, k = 1 bigram, the version combine unigram and bigram
-k = float(0.4)
-backoff = float(0)
-lamf = 0.0026
-nxy = {}
 optparser = optparse.OptionParser()
 optparser.add_option("-c", "--unigramcounts", dest='counts1w', default=os.path.join('data', 'count_1w.txt'), help="unigram counts")
 optparser.add_option("-b", "--bigramcounts", dest='counts2w', default=os.path.join('data', 'count_2w.txt'), help="bigram counts")
 optparser.add_option("-i", "--inputfile", dest="input", default=os.path.join('data', 'input'), help="input file to segment")
-optparser.add_option("-u", "--unibigram", dest="unibigram", default=0.4, help="u == 0: pure unigram")
+optparser.add_option("-k", "--lambda", dest="jm_lambda", default='0.9', help="Lambda for Jelinek-Mercer Smoothing")
+optparser.add_option("-d", "--delta", dest="delta", default=0.001, help="coeficient for bigram additive smoothing")
 (opts, _) = optparser.parse_args()
 
-class PdistUnigram(dict):
+class Pdist(dict):
     "A probability distribution estimated from counts in unigram."
 
     def __init__(self, filename, sep='\t', N=None, missingfn=None):
         self.maxlen = 0
+        self.V = 0  # the number of words in vocabulary
+
         for line in file(filename):
+            self.V += 1
             (key, freq) = line.split(sep)
             try:
                 utf8key = unicode(key, 'utf-8')
@@ -29,54 +28,34 @@ class PdistUnigram(dict):
             self[utf8key] = self.get(utf8key, 0) + int(freq)
             self.maxlen = max(len(utf8key), self.maxlen)
         self.N = float(N or sum(self.itervalues()))
-        self.missingfn = missingfn or (lambda k, N: 1./N*(lamf**(k-1)))
-
-    def __call__(self, key):
-        tempKey = unicode(key.encode("utf8").replace('\xc2\xb7', '1'),"utf8")
-        if unicodedata.normalize('NFKC', tempKey).isdigit():
-            return len(unicodedata.normalize('NFKC', tempKey))/float(self.N)
-
-        if key in self: return float(self[key])/float(self.N)
-        elif len(key) <= 3: return self.missingfn(len(key), self.N)
-        else: return None
-
-class PdistBigram(dict):
-    "A probability distribution estimated from counts in bigram and unigram."
-    def __init__(self, filename, pwUnigram, sep='\t', N=None, missingfn=None):
-        self.maxlen = 0 
-        self.pwUnigram = pwUnigram
-        for line in file(filename):
-            (key, freq) = line.split(sep)
-            (key1, key2) = key.split()
-            try:
-                utf8key1 = unicode(key1, 'utf-8')
-                utf8key2 = unicode(key2, 'utf-8')
-            except:
-                raise ValueError("Unexpected error %s" % (sys.exc_info()[0]))
-            self[utf8key1,utf8key2] = self.get((utf8key1,utf8key2), 0) + int(freq)
-            self.maxlen = max(len(utf8key1), len(utf8key2), self.maxlen)
-            if utf8key1 in nxy:
-                nxy[utf8key1] += 1
-            else:
-                nxy[utf8key1] = 1
-        self.N = float(N or sum(self.itervalues()))
         self.missingfn = missingfn or (lambda k, N: 1./N)
 
     def __call__(self, key):
-        if key in self: return float(self[key]-backoff)/(float(self.N) * float(self.pwUnigram(key[0])))
-        elif key[0] in nxy: return backoff*nxy[key[0]] * self.pwUnigram(key[1])
-        else: return self.pwUnigram(key[1])
+        # Heuristics for digits (also try normalizing full-width digits)
+        tempKey = unicodedata.normalize(
+            'NFKC',
+            # Replace full-width dots with 0 to form consecutive digits.
+            unicode(key.encode("utf8").replace('\xc2\xb7', '0'), "utf8")
+        )
+        if tempKey.isdigit():
+            return 1./self.N
+
+        if key in self:
+            return float(self[key])/float(self.N)
+        else:
+            return self.missingfn(key, self)
+
 
 class Segmenter:
     "Perform segmentation to the input"
 
-    def __init__(self, pwUnigram, pwBigram, maxlen, unibigram):
+    def __init__(self, pwUnigram, pwBigram, pwCombine, maxlen=None):
         self.pwUnigram = pwUnigram
         self.pwBigram = pwBigram
-        self.maxlen = maxlen
-        self.ub = unibigram
+        self.pwCombine = pwCombine
+        self.maxlen = maxlen or self.pwUnigram.maxlen
 
-    def __call__(self, input):
+    def segment(self, input):
         self.scores = {}
         self.prev = {}
         self.heap = []
@@ -111,12 +90,9 @@ class Segmenter:
             for i in range(min(len(input) - 1 - endindex, self.maxlen)):
                 newword = input[endindex + 1: endindex + 2 + i]
                 if self.pwUnigram(newword) is not None:
-                    biprobability = self.pwBigram((entry[1], newword))
-                    uniprobability = self.pwUnigram(newword)
-                    probability = self.ub * biprobability + (1 - self.ub) * uniprobability
-
                     newentry = (endindex + 1, newword)
-                    newscore = self.scores[entry] + math.log(probability, 2)
+                    newscore = self.scores[entry] + \
+                        math.log(self.pwCombine(self.pwUnigram, self.pwBigram, newword, entry[1]))
                     if newentry not in self.heap:
                         heappush(self.heap, newentry)
                         self.scores[newentry] = newscore
@@ -135,10 +111,42 @@ class Segmenter:
         return self.ans
 
 
+# Note: using naive_limit_to_one as missingfn always gives worse result.
+def naive_punish_long_words(key, Pw):
+    # Cut off extremely long (and also unseen) words, which are super unlikely.
+    if len(key) > 10:
+        return None
+    # 1 is also the minimal count in unigram.
+    return (1. / Pw.N) ** len(key)
 
-pwUnigram  = PdistUnigram(opts.counts1w)
-pwBigram = PdistBigram(opts.counts2w, pwUnigram)
-segmenter = Segmenter(pwUnigram, pwBigram, pwUnigram.maxlen, float(opts.unibigram))
+
+def bigram_missingfn(key, Pw):
+    return 0
+
+
+def additive_smoothing(cab, ca, ttl):
+    delta = float(opts.delta)
+    return float(cab + delta) / float(ca + delta * ttl)
+
+
+# Lambda = 0.9 gives the best result, which is the default.
+def cPw_jm_smoothing(Pw, P2w, current_word, prev_word):
+    Lambda = float(opts.jm_lambda)
+    cab = 0
+    if prev_word in Pw:
+        cab = Pw[prev_word]
+    ca = 0
+    if prev_word + ' ' + current_word in P2w:
+        ca = P2w[prev_word + ' ' + current_word]
+
+    p2 = additive_smoothing(cab, ca, Pw.V)
+
+    return p2 * Lambda + Pw(current_word) * (1 - Lambda)
+
+
+pwUnigram = Pdist(opts.counts1w, missingfn=naive_punish_long_words)
+pwBigram = Pdist(opts.counts2w, missingfn=bigram_missingfn)
+segmenter = Segmenter(pwUnigram, pwBigram, cPw_jm_smoothing)
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 
@@ -146,5 +154,5 @@ sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
 with open(opts.input) as f:
     for line in f:
         utf8line = unicode(line.strip(), 'utf-8')
-        print(" ".join(segmenter(utf8line)))
+        print(" ".join(segmenter.segment(utf8line)))
 sys.stdout = old
