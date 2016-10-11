79c1c48869da9b5eee9945f73faacec26267fa1f
ChaunceyKiwi <chaunceykiwi@gmail.com>
2016-10-11 14:21:35 -0700

    add comment

--- chunker/answer/avg_chunker_ch.py
+++ chunker/answer/avg_chunker_ch.py
@@ -19,57 +19,75 @@ def global_feature_vector(feat_list, tag_list):
 def perc_train(train_data, tagset, numepochs):
     feat_vec = defaultdict(int)
     sigma = defaultdict(float)
-
     default_tag = tagset[0]
     tau = {}
-
+    T = numepochs - 1
+    m = len(train_data) - 1
+    
     for t in range(numepochs):
         error_num = 0
-        T = numepochs - 1
-        for i in range(len(train_data)):
-            m = len(train_data) - 1
+        for i in range(len(train_data)):         
             (labeled_list, feat_list) = train_data[i]
             output = perc.perc_test(feat_vec, labeled_list, feat_list, tagset, default_tag)
             expected = [exp.split()[2] for exp in labeled_list]
+
+            # If not the last sentence or not in the last iteration
             if ((t != T) or (i != m)):     
                 if output != expected:
                     vec_output = global_feature_vector(feat_list, output)
                     vec_expected = global_feature_vector(feat_list, expected)
+                    
+                    # Calculate difference between output and expcted result    
                     for vec in vec_output:
                         vec_expected[vec] = vec_expected.get(vec, 0) - vec_output[vec]
+
+                    # include the weight calculated from comparing output and expcted result
                     for vec in vec_expected:
+                        
+                        # Include the total weight during the time
                         if vec in tau:
                             sigma[vec] = sigma.get(vec, 0) + feat_vec[vec] * (t * m + i - tau[vec][1] * m - tau[vec][0])
                         feat_vec[vec] += vec_expected[vec]
                         sigma[vec] += vec_expected[vec] 
+
+                        # Record the location where the dimension s is updated
                         tau[vec] = (i, t)
+
                     error_num += 1
+
+            # To deal with the last sentence in the last iteration
             else:    
+                # Include the total weight during the time
                 for vec in tau:
                     sigma[vec] = sigma.get(vec, 0) + feat_vec[vec] * (T * m + m - tau[vec][1] * m - tau[vec][0])
+
                 if output != expected:
                     vec_output = global_feature_vector(feat_list, output)
                     vec_expected = global_feature_vector(feat_list, expected)
+
+                    # Calculate difference between output and expcted result    
                     for vec in vec_output:
                         vec_expected[vec] = vec_expected.get(vec, 0) - vec_output[vec]
+
+                    # include the weight calculated from comparing output and expcted result
                     for vec in vec_expected:
                         if vec in tau:
                             sigma[vec] = sigma.get(vec, 0) + feat_vec[vec] * (t * m + i - tau[vec][1] * m - tau[vec][0])
                         feat_vec[vec] += vec_expected[vec]
-                        sigma[vec] += vec_expected[vec] 
-                    error_num += 1
-            print error_num
-        print "Number of mistakes: ", error_num
-    sigma = {k: v / (numepochs * len(train_data)) for (k, v) in sigma.items()}
+                        sigma[vec] += vec_expected[vec]
 
-    return sigma
+                    error_num += 1
+
+        print "Number of mistakes: ", error_num
+
+    return {k: v / (numepochs * len(train_data)) for (k, v) in sigma.items()}
 
 if __name__ == '__main__':
     optparser = optparse.OptionParser()
     optparser.add_option("-t", "--tagsetfile", dest="tagsetfile", default=os.path.join("data", "tagset.txt"), help="tagset that contains all the labels produced in the output, i.e. the y in \phi(x,y)")
     optparser.add_option("-i", "--trainfile", dest="trainfile", default=os.path.join("data", "train.txt.gz"), help="input data, i.e. the x in \phi(x,y)")
     optparser.add_option("-f", "--featfile", dest="featfile", default=os.path.join("data", "train.feats.gz"), help="precomputed features for the input data, i.e. the values of \phi(x,_) without y")
-    optparser.add_option("-e", "--numepochs", dest="numepochs", default=int(5), help="number of epochs of training; in each epoch we iterate over over all the training examples")
+    optparser.add_option("-e", "--numepochs", dest="numepochs", default=int(9), help="number of epochs of training; in each epoch we iterate over over all the training examples")
     optparser.add_option("-m", "--modelfile", dest="modelfile", default=os.path.join("data", "default.model"), help="weights for all features stored on disk")
     (opts, _) = optparser.parse_args()
 
