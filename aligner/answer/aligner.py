f210a7486d8270571801ce86680f474532e0d5d5
Robert Ma (Hexcles) <bob1211@gmail.com>
2016-10-27 22:12:22 -0700

    Renaming

--- aligner/answer/aligner.py
+++ aligner/answer/aligner.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import optparse, sys, os, logging
 from collections import defaultdict
-import math
 
 optparser = optparse.OptionParser()
 optparser.add_option("-d", "--datadir", dest="datadir", default="data", help="data directory (default=data)")
@@ -9,7 +8,7 @@ optparser.add_option("-p", "--prefix", dest="fileprefix", default="hansards", he
 optparser.add_option("-e", "--english", dest="english", default="en", help="suffix of English (target language) filename (default=en)")
 optparser.add_option("-f", "--french", dest="french", default="fr", help="suffix of French (source language) filename (default=fr)")
 optparser.add_option("-l", "--logfile", dest="logfile", default=None, help="filename for logging output")
-optparser.add_option("-i", "--max_iterations", dest="max_iters", default=sys.maxint, type="int", help="max number of iterations for training (if omitted, train until convergence)")
+optparser.add_option("-i", "--max_iterations", dest="max_iters", default=5, type="int", help="number of iterations for training")
 optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to use for training and alignment")
 (opts, _) = optparser.parse_args()
 f_data = "%s.%s" % (os.path.join(opts.datadir, opts.fileprefix), opts.french)
@@ -18,7 +17,7 @@ e_data = "%s.%s" % (os.path.join(opts.datadir, opts.fileprefix), opts.english)
 if opts.logfile:
     logging.basicConfig(filename=opts.logfile, filemode='w', level=logging.INFO)
 
-sys.stderr.write("Training with baseline algorithm...\n")
+sys.stderr.write("Training IBM Model 1 with one null word and smoothing...\n")
 bitext = [[sentence.strip().lower().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:opts.num_sents]]
 
 t_fe = defaultdict(float)
@@ -32,58 +31,56 @@ for (f, e) in bitext:
         f_voc.add(f_i)
 V_f = len(f_voc)
 
+# a magic string that never appears in the text
+null_word = '__NULL__'
+
+# add_n value for smoothing
+add_n = 0.01
+
 # initialize t0 uniformly
 for (f, e) in bitext:
     for f_i in set(f):
-        for e_j in set(e):
+        for e_j in set(e).union({null_word}):
             t_fe[(f_i, e_j)] = 1.0 / V_f
 
 # calculate probabilities
-L_old = -float('inf')
 epoch = 0
 while epoch < opts.max_iters:
+    sys.stderr.write("Iteration %d " % (epoch))
+
     count_fe.clear()
     count_e.clear()
     for (f, e) in bitext:
         for f_i in f:
-            z = 0.0
+            # null_word is specially calculated instead of using
+            # `e + [null_word]` for better performance
+            z = t_fe[(f_i, null_word)]
             for e_j in e:
                 z += t_fe[(f_i, e_j)]
+            c = t_fe[(f_i, null_word)] / z
+            count_fe[(f_i, null_word)] += c
+            count_e[null_word] += c
             for e_j in e:
                 c = t_fe[(f_i, e_j)] / z
                 count_fe[(f_i, e_j)] += c
                 count_e[e_j] += c
 
     for (f_i, e_j) in count_fe:
-        t_fe[(f_i, e_j)] = count_fe[(f_i, e_j)] / count_e[e_j]
+        t_fe[(f_i, e_j)] = (count_fe[(f_i, e_j)] + add_n) / (count_e[e_j] + add_n * V_f)
 
-    L_new = 0.0
-    for (n, (f, e)) in enumerate(bitext):
-        Pr = 1.0
-        for f_i in set(f):
-            pr = 0.0
-            for e_j in set(e):
-                pr += t_fe[f_i, e_j]
-
-            Pr *= pr
-
-        L_new += math.log(Pr)
-
-    if L_new - L_old < 10**(-4):
-        break
-
-    L_old = L_new
-    sys.stderr.write("Iteration %d, L=%f\n" % (epoch, L_new))
+    sys.stderr.write("done\n")
     epoch += 1
 
 # decode
+sys.stderr.write("Decoding...\n")
 for (f, e) in bitext:
     for i, f_i in enumerate(f):
-        bestp = 0
-        bestj = 0
+        bestp = t_fe[(f_i, null_word)]
+        bestj = -1
         for j, e_j in enumerate(e):
             if t_fe[(f_i, e_j)] > bestp:
                 bestp = t_fe[(f_i, e_j)]
                 bestj = j
-        sys.stdout.write("%i-%i " % (i, bestj))
+        if bestj >= 0:
+            sys.stdout.write("%i-%i " % (i, bestj))
     sys.stdout.write("\n")
