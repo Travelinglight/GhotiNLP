9dfa6d9558d4eb825c40cc61869c594ebceb7ea8
Robert Ma (Hexcles) <bob1211@gmail.com>
2016-12-14 12:55:14 -0800

    Minor changes in feedback_loop

--- project/src/feedback_loop.py
+++ project/src/feedback_loop.py
@@ -15,26 +15,31 @@ optparser.add_option("-t", "--translation-model-train", dest="tmdev", default="d
 optparser.add_option("-u", "--translation-model-test", dest="tmtest", default="data/large/phrase-table/test-filtered/rules_cnt.final.out", help="File containing translation model (default=data/tm)")
 optparser.add_option("-l", "--language-model", dest="lm", default="data/lm/en.gigaword.3g.filtered.train_dev_test.arpa.gz", help="File containing ARPA-format language model (default=data/lm)")
 optparser.add_option("-k", "--translations-per-phrase", dest="k", default=3, type="int", help="Limit on number of translations to consider per phrase (default=1)")
-optparser.add_option("-s", "--stack-size", dest="s", default=100, type="int", help="Maximum stack size (default=1)")
-optparser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False,  help="Verbose mode (default=off)")
-optparser.add_option("-f", "--feedback-loop", dest="loop", default=10,  help="The number of times the weight vector loops between decoder and reranker")
-optparser.add_option("-c", "--simplify", dest="simplify", default=True,  help="Simplified mode (default=on)")
+optparser.add_option("-s", "--stack-size", dest="s", default=10, type="int", help="Maximum stack size (default=10)")
+optparser.add_option("-f", "--feedback-loop", dest="loop", default=5, type="int", help="The number of times the weight vector loops between decoder and reranker")
+optparser.add_option("--simplify", dest="simplify", action="store_true", default=True, help="Simplified mode (default=on)")
+optparser.add_option("--resegment-unknown", dest="reseg_unknown", action="store_true", default=False, help="Try to resegment unknown words into two known words (default=off)")
+optparser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False, help="Verbose mode (default=off)")
 
 opts = optparser.parse_args()[0]
 
 lm = models.LM(opts.lm)
 
+# Training
 weights = [1.0 / 6] * 6
 for i in range(int(opts.loop)):
     tm = models.TM(opts.tmdev, opts.k, weights[:4], simpmode=opts.simplify)
-    nbest_list = list(decode.get_candidates(opts.input, tm, lm, weights, stack_size=opts.s, verbose=opts.verbose, simpmode=opts.simplify))
+    nbest_list = list(decode.get_candidates(opts.input, tm, lm, weights, stack_size=opts.s, verbose=opts.verbose, simpmode=opts.simplify, separate_unknown_words=opts.reseg_unknown))
     weights = trainreranker.train(nbest_list, opts.reference, weights)
     print weights
     results = rerank.rerank(weights, nbest_list)
-    print >> sys.stderr, "BLEU SCORE: %f:" % scorereranker.score(results, opts.reference)
+    print >> sys.stderr, "TRAINING LOOP %d BLEU SCORE: %f:" % \
+        (i, scorereranker.score(results, opts.reference))
 
-tm = models.TM(opts.tmtest, opts.k, weights[:4])
-nbest_list = decode.get_candidates(opts.eval, tm, lm, weights, nbest=1)
-#results = rerank.rerank(weights, nbest_list)
+# Testing
+tm = models.TM(opts.tmtest, opts.k, weights[:4], simpmode=opts.simplify)
+nbest_list = list(decode.get_candidates(opts.eval, tm, lm, weights, nbest=1, stack_size=100, verbose=opts.verbose, simpmode=opts.simplify, separate_unknown_words=opts.reseg_unknown))
+print >> sys.stderr, "TEST BLEU SCORE: %f:" % scorereranker.score(results, opts.reference)
 with open("output1", "w") as f:
     f.write("\n".join(results))
+print >> sys.stderr, "Results written to output1"
