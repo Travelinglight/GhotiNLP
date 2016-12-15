efc18c67bbd50616bf45b983c2fd2794a771bb95
Robert Ma (Hexcles) <bob1211@gmail.com>
2016-12-14 16:26:51 -0800

    Add a warning to reranker

--- project/src/rerank.py
+++ project/src/rerank.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 from collections import namedtuple
 
 def rerank(w, nbestlist):
@@ -13,5 +14,11 @@ def rerank(w, nbestlist):
         assert len(w) == len(features)
         nbests[int(i)].append(translation(sentence.strip(), sum([x*y for x,y in zip(w, features)])))
 
-    results = [max(nbest, key=lambda x: x.score).english for nbest in nbests]
+    results = []
+    for n, nbest in enumerate(nbests):
+        if len(nbest) == 0:
+            print >> sys.stderr, "WARNING: no translation found for line %d" % (n+1)
+            results.append('')
+        else:
+            results.append(max(nbest, key=lambda x: x.score).english)
     return results
