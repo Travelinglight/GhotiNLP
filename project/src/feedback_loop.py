26e4c8566fd0c787a207a8041d5af1dc22f34553
Robert Ma (Hexcles) <bob1211@gmail.com>
2016-12-14 16:49:33 -0800

    Change default stack size

--- project/src/feedback_loop.py
+++ project/src/feedback_loop.py
@@ -15,7 +15,7 @@ optparser.add_option("-t", "--translation-model-train", dest="tmdev", default="d
 optparser.add_option("-u", "--translation-model-test", dest="tmtest", default="data/large/phrase-table/test-filtered/rules_cnt.final.out", help="File containing translation model (default=data/tm)")
 optparser.add_option("-l", "--language-model", dest="lm", default="data/lm/en.gigaword.3g.filtered.train_dev_test.arpa.gz", help="File containing ARPA-format language model (default=data/lm)")
 optparser.add_option("-k", "--translations-per-phrase", dest="k", default=3, type="int", help="Limit on number of translations to consider per phrase (default=1)")
-optparser.add_option("-s", "--stack-size", dest="s", default=10, type="int", help="Maximum stack size (default=10)")
+optparser.add_option("-s", "--stack-size", dest="s", default=100, type="int", help="Maximum stack size (default=10)")
 optparser.add_option("-f", "--feedback-loop", dest="loop", default=5, type="int", help="The number of times the weight vector loops between decoder and reranker")
 optparser.add_option("--simplify", dest="simplify", action="store_true", default=False, help="Simplified mode (default=off)")
 optparser.add_option("--resegment-unknown", dest="reseg_unknown", action="store_true", default=False, help="Try to resegment unknown words into two known words (default=off)")
@@ -27,7 +27,7 @@ lm = models.LM(opts.lm)
 
 # Training
 weights = [1.0 / 7] * 7
-for i in range(int(opts.loop)):
+for i in range(loop):
     tm = models.TM(opts.tmdev, opts.k, weights[:4], simpmode=opts.simplify)
     nbest_list = list(decode.get_candidates(opts.input, tm, lm, weights, stack_size=opts.s, verbose=opts.verbose, simpmode=opts.simplify, separate_unknown_words=opts.reseg_unknown))
     weights = trainreranker.train(nbest_list, opts.reference, weights)
